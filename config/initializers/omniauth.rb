c07c0e6350b438481bfd0320b270a1e0ce575605
johnh4 <jehowl4@gmail.com>
2014-07-09 20:35:45 -0400

    Initial commit.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+=begin
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :moves, 's8HaqOeW_UP63PGBe064PowFaSPdakm3',
+									 '6RsSfc1LMSItlqF0c62mD5VdkFswIMKTjrkiXjDChe7153TKsG8H0P52v_dy8zay'
+end
+=end
