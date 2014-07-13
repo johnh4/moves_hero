b2400d8d8297e0d707a1c8695ac62daf071b8927
johnh4 <jehowl4@gmail.com>
2014-07-13 19:48:00 -0400

    Add Procfile, refactor home controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,42 +3,15 @@ class HomeController < ApplicationController
 		if user_signed_in? && current_user.valid_token?
 			token = current_user.moves_token
 			@moves = Move.new(token)
-			#@summary = @moves.daily_summary
-			#@month_summary = @moves.month_summary
-			#@month_steps = @moves.month_steps
-			#@month_days = @moves.month_days
 			@walk_month_summary = @moves.walk_month_summary.reverse
 			@movers_daily = User.movers.order(steps_today: :desc)
 			@movers_monthly = User.movers.order(steps_month: :desc)
 		end
   end
-	def leaderboard
-		@movers_daily = User.movers.order(steps_today: :desc)
-		@movers_monthly = User.movers.order(steps_month: :desc)
-		if user_signed_in? && current_user.valid_token?
-			@token = current_user.moves_token
-			@moves = Move.new(@token)
-			@summary = @moves.daily_summary
-			@month_summary = @moves.month_summary
-			@month_steps = @moves.month_steps
-			@month_days = @moves.month_days
-			puts "@moves: #{@moves}"
-			puts "@month_days before: #{@month_days}"
-			puts "@month_days after: #{@month_days}"
-			puts "@moves.profile: #{@moves.profile}"
-			puts "@moves.month_summary(2014-07): #{@moves.daily_summary('2014-07')}"
-			puts "@moves.daily_summary(Time.now): #{@moves.daily_summary(Time.now)}"
-
-			@walk_month_summary = @moves.walk_month_summary.reverse
-		end
-	end
-
 	def month
 		if user_signed_in? && current_user.valid_token?
 			token = current_user.moves_token
 			@moves = Move.new(token)
-			#@summary = @moves.daily_summary
-			#@month_summary = @moves.month_summary
 			@month_steps = @moves.month_steps
 			@month_days = @moves.month_days
 		end
